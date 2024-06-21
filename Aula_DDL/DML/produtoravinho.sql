dae3cce1c5fa02cd9c8e406082de7f5c6d9bda0a
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-06-21 12:39:19 -0300

    Add files via upload

---
+++ Aula_DDL/DML/produtoravinho.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 21/06/2024 às 17:37
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `produtoravinho`
+--
+CREATE DATABASE IF NOT EXISTS `produtoravinho` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `produtoravinho`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `casta`
+--
+
+CREATE TABLE `casta` (
+  `casta_id` int(11) NOT NULL,
+  `casta` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `casta`
+--
+
+INSERT INTO `casta` (`casta_id`, `casta`) VALUES
+(1, 'Touringa Nacional'),
+(2, 'Tinta Rodriz'),
+(3, 'Rosé de Merlot'),
+(4, 'Rosé de Pinot'),
+(5, 'Zinfandel'),
+(6, 'Grenache');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `casta_vinho`
+--
+
+CREATE TABLE `casta_vinho` (
+  `codVinho` int(11) DEFAULT NULL,
+  `codCasta` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `casta_vinho`
+--
+
+INSERT INTO `casta_vinho` (`codVinho`, `codCasta`) VALUES
+(1, 1),
+(2, 1),
+(3, 4),
+(6, 2),
+(5, 3),
+(2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtor`
+--
+
+CREATE TABLE `produtor` (
+  `produtor_id` int(11) NOT NULL,
+  `produtor` varchar(50) DEFAULT NULL,
+  `moradaProdutor` varchar(50) DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `codregiao` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `produtor`
+--
+
+INSERT INTO `produtor` (`produtor_id`, `produtor`, `moradaProdutor`, `telefone`, `email`, `codregiao`) VALUES
+(1, 'Qt. Vallado', 'Régua', '254323147', 'vallado@mail.telepac.pt', 1),
+(2, 'Finagra', 'Reguegos', '266509270', 'esporao@esporao.com', 3),
+(3, 'Vasco', 'Pindamonhagaba', '11 9293-9393', 'euzinho@gmail.com', 4),
+(4, 'Jhonathan', 'Jacaré Pagua', '16 78328-9877', 'vamonessa@gmail.com', 5),
+(5, 'Tony', 'São Paulo', '99 9999-9999', 'ttut@gmail.com', 6),
+(6, 'Marajo', 'Minas Gerais', '55 9347-0987', 'lesgobreazilcopa@gmail.com', 3),
+(7, 'Fidalgo', 'Indonésia', '45 4545-4545', 'ramons@gmail.com', 2),
+(8, 'Mané', 'Ribeirão Pires', '9090 0908098-99', 'valameudeus@gmail.com', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `regiao`
+--
+
+CREATE TABLE `regiao` (
+  `idregiao` int(11) NOT NULL,
+  `regiao` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `regiao`
+--
+
+INSERT INTO `regiao` (`idregiao`, `regiao`) VALUES
+(1, 'Douro'),
+(2, 'Alentejo'),
+(3, 'Dão'),
+(4, 'Fazenda do Sul'),
+(5, 'Piraporinha do Oeste'),
+(6, 'Fazenda MCM');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vinho`
+--
+
+CREATE TABLE `vinho` (
+  `vinho_id` int(11) NOT NULL,
+  `vinho` varchar(30) DEFAULT NULL,
+  `ano_vinho` int(11) DEFAULT NULL,
+  `cor` varchar(15) DEFAULT NULL,
+  `grau` decimal(7,2) DEFAULT NULL,
+  `preco` decimal(7,2) DEFAULT NULL,
+  `codProdutor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `vinho`
+--
+
+INSERT INTO `vinho` (`vinho_id`, `vinho`, `ano_vinho`, `cor`, `grau`, `preco`, `codProdutor`) VALUES
+(1, 'Esporão Reserva', 2004, 'Tinto', 14.50, 18.50, 3),
+(2, 'Quinta do Vallado', 2004, 'Tinto', 14.00, 6.50, 1),
+(3, 'Muros Antigos', 2006, 'Branco', 13.00, 7.50, 7),
+(4, 'Piscines Stripes', 1998, 'Rose', 11.00, 86.50, 8),
+(5, 'Mateus Rose', 2009, 'Tinto', 5.00, 96.80, 4),
+(6, 'Canonico Salton', 700, 'Branco', 13.00, 9999.70, 5);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `casta`
+--
+ALTER TABLE `casta`
+  ADD PRIMARY KEY (`casta_id`);
+
+--
+-- Índices de tabela `casta_vinho`
+--
+ALTER TABLE `casta_vinho`
+  ADD KEY `codVinho` (`codVinho`),
+  ADD KEY `codCasta` (`codCasta`);
+
+--
+-- Índices de tabela `produtor`
+--
+ALTER TABLE `produtor`
+  ADD PRIMARY KEY (`produtor_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `codregiao` (`codregiao`);
+
+--
+-- Índices de tabela `regiao`
+--
+ALTER TABLE `regiao`
+  ADD PRIMARY KEY (`idregiao`);
+
+--
+-- Índices de tabela `vinho`
+--
+ALTER TABLE `vinho`
+  ADD PRIMARY KEY (`vinho_id`),
+  ADD KEY `codProdutor` (`codProdutor`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `casta`
+--
+ALTER TABLE `casta`
+  MODIFY `casta_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `produtor`
+--
+ALTER TABLE `produtor`
+  MODIFY `produtor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de tabela `regiao`
+--
+ALTER TABLE `regiao`
+  MODIFY `idregiao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `vinho`
+--
+ALTER TABLE `vinho`
+  MODIFY `vinho_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `casta_vinho`
+--
+ALTER TABLE `casta_vinho`
+  ADD CONSTRAINT `casta_vinho_ibfk_1` FOREIGN KEY (`codVinho`) REFERENCES `vinho` (`vinho_id`),
+  ADD CONSTRAINT `casta_vinho_ibfk_2` FOREIGN KEY (`codCasta`) REFERENCES `casta` (`casta_id`);
+
+--
+-- Restrições para tabelas `produtor`
+--
+ALTER TABLE `produtor`
+  ADD CONSTRAINT `produtor_ibfk_1` FOREIGN KEY (`codregiao`) REFERENCES `regiao` (`idregiao`);
+
+--
+-- Restrições para tabelas `vinho`
+--
+ALTER TABLE `vinho`
+  ADD CONSTRAINT `vinho_ibfk_1` FOREIGN KEY (`codProdutor`) REFERENCES `produtor` (`produtor_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
