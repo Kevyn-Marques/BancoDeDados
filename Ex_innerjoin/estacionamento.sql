374cea66acfd95ac0ce8d0817c222e73ad4f9758
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-09-23 08:30:11 -0300

    Add files via upload

--- Ex_innerjoin/estacionamento.sql
+++ Ex_innerjoin/estacionamento.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16-Set-2024 às 14:05
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.0.25
+-- Tempo de geração: 23-Set-2024 às 13:28
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,7 +33,7 @@ CREATE TABLE `andar` (
   `id_andar` int(11) NOT NULL,
   `capacidade` int(11) DEFAULT NULL,
   `andar` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `andar`
@@ -71,7 +71,7 @@ CREATE TABLE `cliente` (
   `cpf` varchar(255) NOT NULL,
   `dataNasc` datetime DEFAULT NULL,
   `nome` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `cliente`
@@ -113,7 +113,31 @@ CREATE TABLE `estaciona` (
   `dtEntrada` datetime DEFAULT NULL,
   `placa` varchar(7) DEFAULT NULL,
   `id_andar` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `estaciona`
+--
+
+INSERT INTO `estaciona` (`id_estaciona`, `horSaida`, `horEntrada`, `dtSaida`, `dtEntrada`, `placa`, `id_andar`) VALUES
+(1, '2024-09-01 08:30:00', '2024-09-01 17:00:00', '2024-09-01 00:00:00', '2024-09-01 00:00:00', 'ABC1D23', 1),
+(2, '2024-09-02 08:45:00', '2024-09-02 17:15:00', '2024-09-02 00:00:00', '2024-09-02 00:00:00', 'XYZ4E56', 2),
+(3, '2024-09-03 09:00:00', '2024-09-03 17:30:00', '2024-09-03 00:00:00', '2024-09-03 00:00:00', 'LMN7F89', 3),
+(4, '2024-09-04 08:15:00', '2024-09-04 16:45:00', '2024-09-04 00:00:00', '2024-09-04 00:00:00', 'OPQ0G12', 4),
+(5, '2024-09-05 09:10:00', '2024-09-05 17:20:00', '2024-09-05 00:00:00', '2024-09-05 00:00:00', 'RST3H45', 5),
+(6, '2024-09-06 08:00:00', '2024-09-06 17:00:00', '2024-09-06 00:00:00', '2024-09-06 00:00:00', 'UVW6I78', 6),
+(7, '2024-09-07 09:30:00', '2024-09-07 17:45:00', '2024-09-07 00:00:00', '2024-09-07 00:00:00', 'ABC9J01', 7),
+(8, '2024-09-08 08:20:00', '2024-09-08 17:10:00', '2024-09-08 00:00:00', '2024-09-08 00:00:00', 'DEF2K34', 8),
+(9, '2024-09-09 08:35:00', '2024-09-09 17:05:00', '2024-09-09 00:00:00', '2024-09-09 00:00:00', 'GHI5L67', 9),
+(10, '2024-09-10 09:40:00', '2024-09-10 17:55:00', '2024-09-10 00:00:00', '2024-09-10 00:00:00', 'MNO1P23', 10),
+(11, '2024-09-11 08:50:00', '2024-09-11 17:25:00', '2024-09-11 00:00:00', '2024-09-11 00:00:00', 'PQR4Q56', 11),
+(12, '2024-09-12 09:15:00', '2024-09-12 17:15:00', '2024-09-12 00:00:00', '2024-09-12 00:00:00', 'STU7R89', 12),
+(13, '2024-09-13 08:05:00', '2024-09-13 17:35:00', '2024-09-13 00:00:00', '2024-09-13 00:00:00', 'VWX0Y12', 13),
+(14, '2024-09-14 09:25:00', '2024-09-14 17:40:00', '2024-09-14 00:00:00', '2024-09-14 00:00:00', 'YZA3B45', 14),
+(15, '2024-09-15 08:10:00', '2024-09-15 17:30:00', '2024-09-15 00:00:00', '2024-09-15 00:00:00', 'BCD6C78', 15),
+(16, '2024-09-16 09:05:00', '2024-09-16 17:50:00', '2024-09-16 00:00:00', '2024-09-16 00:00:00', 'EFG9D01', 16),
+(17, '2024-09-17 08:55:00', '2024-09-17 17:10:00', '2024-09-17 00:00:00', '2024-09-17 00:00:00', 'HIJ2E34', 17),
+(18, '2024-09-18 09:35:00', '2024-09-18 17:20:00', '2024-09-18 00:00:00', '2024-09-18 00:00:00', 'KLM5F67', 18);
 
 -- --------------------------------------------------------
 
@@ -124,7 +148,7 @@ CREATE TABLE `estaciona` (
 CREATE TABLE `modelo` (
   `id_modelo` int(11) NOT NULL,
   `modelo` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `modelo`
@@ -163,7 +187,7 @@ CREATE TABLE `veiculo` (
   `cor` varchar(20) DEFAULT NULL,
   `cpf` varchar(255) DEFAULT NULL,
   `id_modelo` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `veiculo`
@@ -242,7 +266,7 @@ ALTER TABLE `andar`
 -- AUTO_INCREMENT de tabela `estaciona`
 --
 ALTER TABLE `estaciona`
-  MODIFY `id_estaciona` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_estaciona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de tabela `modelo`
